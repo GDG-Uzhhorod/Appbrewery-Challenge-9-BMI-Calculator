0ee2c98720d5d3e8fb673fbe84bad2ae37822613
Brusd <brusd.com@gmail.com>
2019-12-27 02:58:28 +0200

    Create Reusable widget with color constant

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+const bottomContainerHeight = 80.0;
+const activeCodeColor = Color(0xFF1D1E33);
+const bottomContainerColor = Color(0xFFEB1555);
+
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
@@ -23,55 +27,61 @@ class _InputPageState extends State<InputPage> {
           child: Row(
             children: <Widget>[
               Expanded(
-                child: Container(
-                  margin: EdgeInsets.all(15.0),
-                  decoration: BoxDecoration(
-                      color: Color(0xFF1D1E33),
-                      borderRadius: BorderRadius.circular(10.0)),
+                child: ReusableCard(
+                  colour: activeCodeColor,
                 ),
               ),
               Expanded(
-                child: Container(
-                  margin: EdgeInsets.all(15.0),
-                  decoration: BoxDecoration(
-                      color: Color(0xFF1D1E33),
-                      borderRadius: BorderRadius.circular(10.0)),
+                child: ReusableCard(
+                  colour: activeCodeColor,
                 ),
               ),
             ],
           ),
         ),
         Expanded(
-          child: Container(
-            margin: EdgeInsets.all(15.0),
-            decoration: BoxDecoration(
-                color: Color(0xFF1D1E33),
-                borderRadius: BorderRadius.circular(10.0)),
+          child: ReusableCard(
+            colour: activeCodeColor,
           ),
         ),
         Expanded(
           child: Row(
             children: <Widget>[
               Expanded(
-                child: Container(
-                  margin: EdgeInsets.all(15.0),
-                  decoration: BoxDecoration(
-                      color: Color(0xFF1D1E33),
-                      borderRadius: BorderRadius.circular(10.0)),
+                child: ReusableCard(
+                  colour: activeCodeColor,
                 ),
               ),
               Expanded(
-                child: Container(
-                  margin: EdgeInsets.all(15.0),
-                  decoration: BoxDecoration(
-                      color: Color(0xFF1D1E33),
-                      borderRadius: BorderRadius.circular(10.0)),
+                child: ReusableCard(
+                  colour: activeCodeColor,
                 ),
               ),
             ],
           ),
         ),
+        Container(
+          color: bottomContainerColor,
+          margin: EdgeInsets.only(top: 10.0),
+          width: double.infinity,
+          height: bottomContainerHeight,
+        )
       ],
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  ReusableCard({@required this.colour});
+
+  final Color colour;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+          color: colour, borderRadius: BorderRadius.circular(10.0)),
     );
   }
 }
